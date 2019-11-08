9f8ed79f8eafb293f3b9760aec552a1e123812f0
Timani G <timanig@gmail.com>
2019-11-08 14:59:33 -0500

    /Unit Testing
    * Test-Driven-Development *
    -----------------------------
    **Summary**
    This program demonstrates unit testing

---
+++ Unit-Testing/Unit-Test/Test.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using UnitTesting;
+namespace UnitTest
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+            Assert.True(true);
+        }
+        [Test()]
+        public void AdditionTests()
+        {
+            //If
+            var x = new Calculations();
+            var y = new Calculations();
+            //When
+            x.X = 3;
+            x.Y = 4;
+            y.X = -3;
+            y.Y = 0;
+            // Then
+            Assert.AreEqual(7, x.Addition(x.X, x.Y));
+            Assert.AreEqual(-3, x.Addition(y.X, y.Y));
+        }
+        [Test()]
+        public void MultiplicationTests()
+        {
+            //If
+            var x = new Calculations();
+            var y = new Calculations();
+
+            //When
+            x.X = 3;
+            x.Y = 4;
+            y.X = -3;
+            y.Y = 4;
+            // Then
+            Assert.AreEqual(12, x.Multiplication(x.X, x.Y));
+            Assert.AreEqual(-12, x.Multiplication(y.X, y.Y));
+        }
+    }
+}
---
+++ Unit-Testing/Unit-Testing/Calculations.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace UnitTesting
+{
+    public class Calculations
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Addition(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Multiplication(int x, int y) => (x * y);
+    }
+}
